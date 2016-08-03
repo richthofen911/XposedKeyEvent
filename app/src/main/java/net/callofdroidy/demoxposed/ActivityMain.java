155813b4d8826f51ba21e11314e20fc6a136988b
richthofen911 <yichaoli.richthofen@gmail.com>
2016-08-03 18:18:00 -0400

    .

--- app/src/main/java/net/callofdroidy/demoxposed/ActivityMain.java
+++ app/src/main/java/net/callofdroidy/demoxposed/ActivityMain.java
@@ -2,12 +2,16 @@ package net.callofdroidy.demoxposed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ActivityMain extends AppCompatActivity {
+    final static String TAG = "ActivityMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/net/callofdroidy/demoxposed/Tutorial.java
+++ app/src/main/java/net/callofdroidy/demoxposed/Tutorial.java
@@ -1,34 +1,42 @@
 package net.callofdroidy.demoxposed;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.os.PowerManager;
 import android.widget.TextView;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 /**
  * Created by admin on 25/07/16.
  */
 public class Tutorial implements IXposedHookLoadPackage{
+
+    @Override
     public void handleLoadPackage(final LoadPackageParam loadPackageParam) throws Throwable{
-        if(!loadPackageParam.packageName.equals("com.android.systemui"))
+        if (!loadPackageParam.packageName.equals("android.media"))
             return;
+        Class<?> audioManager = XposedHelpers.findClass("android.media.AudioManager", loadPackageParam.classLoader);
+        XposedBridge.hookAllMethods(audioManager, "handleKeyDown", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Context context = (Context)param.getResult();
+                PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+                if(!powerManager.isScreenOn()){
+                    PowerManager.WakeLock wl = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE,"MyLock");
 
-        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", loadPackageParam.classLoader,
-                "updateClock", new XC_MethodHook() {
+                    wl.acquire(10000);
+                    PowerManager.WakeLock wl_cpu = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"MyCpuLock");
 
-                    @Override
-                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        TextView tv = (TextView) param.thisObject;
-                        String text = tv.getText().toString();
-                        text = text + " :)";
-                        tv.setText(text);
-                        tv.setTextColor(Color.RED);
-                    }
-                });
-        XposedBridge.log("We are in SystemUI!");
+                    wl_cpu.acquire(10000);
+                }
+            }
+        });
+
     }
 }
